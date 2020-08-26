33bae8e514bb4c48f73eac02b51cdec835a4f72c
csenw <sen.wang1982@gmail.com>
2020-08-26 14:44:28 +0000

    third

--- src/index.php
+++ src/index.php
@@ -1,8 +1,4 @@
 <?php 
 echo "I am a PHP page running on four containers: NginX + PHP-PFM + MariaDB + Redis!";
-$link = mysql_connect('mysql', 'php', 'php');
-if (!$link) {
-    die('Could not connect: ' . mysql_error());
-}
-printf("MySQL server version: %s\n", mysql_get_server_info());
-phpinfo(); ?>
+phpinfo();
+?>
