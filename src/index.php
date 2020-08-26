07d47ca478e058a30e0745d7a53ac8d68d0dd507
csenw <sen.wang1982@gmail.com>
2020-08-26 23:59:37 +1000

    second update

--- src/index.php
+++ src/index.php
@@ -1,3 +1,8 @@
 <?php 
-echo "I am a PHP page running on NginX + PHP-PFM in a container!";
+echo "I am a PHP page running on four containers: NginX + PHP-PFM + MariaDB + Redis!";
+$link = mysql_connect('mysql', 'php', 'php');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+printf("MySQL server version: %s\n", mysql_get_server_info());
 phpinfo(); ?>
