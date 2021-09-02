fb184c1843aea8db1398c27ed1868f96046d5f03
Sen <sen.wang1982@gmail.com>
2021-09-02 16:02:02 +1000

    Update order.php

--- src/order.php
+++ src/order.php
@@ -7,6 +7,6 @@ echo "Connection to Redis server sucessfully!";
 echo "<br>";
 echo "Server is running: " . $redis->ping();
 echo "<br>";
-$redis->set("Cloud Computing", "INFS3208/7208"); 
+$redis->set("Cloud Computing", "INFS3208"); 
 echo "Course code of Cloud Computing is: " .$redis->get("Cloud Computing"); 
 ?>
